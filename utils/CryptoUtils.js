34ea037950d0ad40678735158c65300dab412bd1
fixorium <fixercoin.token@gmail.com>
2025-07-06 01:29:29 +0500

    Update CryptoUtils.js

--- utils/CryptoUtils.js
+++ utils/CryptoUtils.js
@@ -1,1 +1,16 @@
+const crypto = require('crypto');
+
+class CryptoUtils {
+  static hash(data) {
+    return crypto.createHash('sha256').update(data).digest('hex');
+  }
+
+  static generateKeyPair() {
+    return crypto.generateKeyPairSync('rsa', {
+      modulusLength: 2048,
+    });
+  }
+}
+
+module.exports = CryptoUtils;
 
