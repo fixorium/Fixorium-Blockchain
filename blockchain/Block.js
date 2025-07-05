0a20567f30f07bd493a612ac26d15b5afa1a4603
fixorium <fixercoin.token@gmail.com>
2025-07-06 01:17:18 +0500

    Update Block.js

--- blockchain/Block.js
+++ blockchain/Block.js
@@ -1,1 +1,12 @@
+class Block {
+  constructor(timestamp, transactions, previousHash = '') {
+    this.timestamp = timestamp;
+    this.transactions = transactions;
+    this.previousHash = previousHash;
+    this.nonce = 0;
+    this.hash = '';
+  }
+}
+
+module.exports = Block;
 
